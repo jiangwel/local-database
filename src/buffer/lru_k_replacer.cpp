2153c8557bc0cabeee48ec7ad6d38cf236897019
Jiangwei 江巍 <84135487+jiangwel@users.noreply.github.com>
2023-06-09 12:14:51 +0000

    refactor:lruk

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -16,20 +16,39 @@
 
 namespace bustub {
 // LRUKReplacer
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : k_(k), replacer_size_(num_frames) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : k_(k), replacer_size_(num_frames) {
+  LOG_INFO("LRUKReplacer: replacer_size_: %zu, k: %zu", num_frames, k);
+}
 
 auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
   std::scoped_lock<std::mutex> lock(latch_);
-  if (evictable_num_ != 0) {
-    std::list<frame_id_t> &frame_id_list = access_less_k_.empty() ? access_k_frame_ : access_less_k_;
-    for (auto it = frame_id_list.begin(); it != frame_id_list.end(); it++) {
-      if (frame_info_[*it].evictable_) {
-        *frame_id = *it;
-        frame_info_.erase(*it);
-        frame_id_list.erase(it);
-        evictable_num_--;
-        return true;
-      }
+  // LOG_INFO("Evict");
+  if (evictable_num_ == 0) {
+    return false;
+  }
+    // delete
+    //  if(access_less_k_.empty()){
+    //    // LOG_INFO("  access_k_frame_!");
+    //  } else {
+    //    // LOG_INFO("  access_less_k_!");
+    //  }
+  std::list<frame_id_t> &frame_id_list = access_less_k_.empty() ? access_k_frame_ : access_less_k_;
+  for (auto it = frame_id_list.begin(); it != frame_id_list.end(); it++) {
+    if (frame_info_[*it].evictable_) {
+      *frame_id = *it;
+      frame_info_.erase(*it);
+      frame_id_list.erase(it);
+      evictable_num_--;
+      // delete
+      //  for(auto it = frame_id_list.begin(); it != frame_id_list.end(); it++){
+      //    // LOG_INFO("    %d", *it);
+      //  }
+      //  // LOG_INFO("  frame_info_");
+      //  for(auto it = frame_info_.begin(); it != frame_info_.end(); it++){
+      //    // LOG_INFO("    id: %d,access time: %zu,evictable: %d ", it->first, it->second.access_time_,
+      //    it->second.evictable_);
+      //  }
+      return true;
     }
   }
   return false;
@@ -37,29 +56,62 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
 
 void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
   std::scoped_lock<std::mutex> lock(latch_);
-  if(frame_id > static_cast<int>(replacer_size_)) {
+  // LOG_INFO("RecordAccess: frame_id: %d", frame_id);
+  if (frame_id > static_cast<int>(replacer_size_)) {
     throw Exception("Can't insert frame any more!");
   }
   if (frame_info_.find(frame_id) == frame_info_.end()) {
-      frame_info_[frame_id] = {1, false};
-      access_less_k_.push_back(frame_id);
-  } else {
-    if (frame_info_[frame_id].access_time_ < k_) {
-      if (++frame_info_[frame_id].access_time_ == k_) {
-        access_k_frame_.push_back(frame_id);
-        access_less_k_.remove(frame_id);
-      }
-    } else {
-      access_k_frame_.remove(frame_id);
+    frame_info_[frame_id] = {1, false};
+    access_less_k_.push_back(frame_id);
+    // // LOG_INFO("  insert");
+    // LOG_INFO("  frame_info_");
+    // // for(auto it = frame_info_.begin(); it != frame_info_.end(); it++){
+    //   LOG_INFO("    id: %d,access time: %zu,evictable: %d ", it->first, it->second.access_time_,
+    //   it->second.evictable_);
+    // // }
+    // LOG_INFO("  access_less_k_");
+    // // for(auto it = access_less_k_.begin(); it != access_less_k_.end(); it++){
+    //   LOG_INFO("    %d", *it);
+    // // }
+  } else if (frame_info_[frame_id].access_time_ < k_) {
+    if (++frame_info_[frame_id].access_time_ == k_) {
       access_k_frame_.push_back(frame_id);
+      access_less_k_.remove(frame_id);
+      // // LOG_INFO("  access_time_ == k_");
+      // LOG_INFO("  access_k_frame_");
+      // // for(auto it = access_k_frame_.begin(); it != access_k_frame_.end(); it++){
+      //   LOG_INFO("    %d", *it);
+      // // }
+      // LOG_INFO("  access_less_k_");
+      // // for(auto it = access_less_k_.begin(); it != access_less_k_.end(); it++){
+      //   LOG_INFO("    %d", *it);
+      // // }
     }
+      // delete
+      //  else{
+      //    // LOG_INFO("  access_time_ < k_");
+      //  }
+  } else {
+    // LOG_INFO("  access_time_ > k_");
+    if(frame_info_.find(frame_id)!=frame_info_.end()){
+      access_k_frame_.remove(frame_id);
+    }
+    access_k_frame_.push_back(frame_id);
+    // LOG_INFO("  access_k_frame_");
+    // //   for(auto it = access_k_frame_.begin(); it != access_k_frame_.end(); it++){
+    //     LOG_INFO("    %u", *it);
+    // //   }
   }
 }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
   std::scoped_lock<std::mutex> lock(latch_);
+  // LOG_INFO("SetEvictable: frame_id: %d, set_evictable: %d", frame_id, set_evictable);
+  if (frame_id > static_cast<int>(replacer_size_)) {
+    throw Exception("SetEvictable:frame_id is invalid!");
+  }
   if (frame_info_.find(frame_id) == frame_info_.end()) {
-    throw Exception("frame_id is invalid!");
+    return;
   }
   if (set_evictable != frame_info_[frame_id].evictable_) {
     frame_info_[frame_id].evictable_ = set_evictable;
@@ -69,15 +121,20 @@ void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
 
 void LRUKReplacer::Remove(frame_id_t frame_id) {
   std::scoped_lock<std::mutex> lock(latch_);
-  if (frame_info_.find(frame_id) != frame_info_.end()) {
-    if (frame_info_[frame_id].evictable_) {
-      std::list<frame_id_t> &frame_id_list = frame_info_[frame_id].access_time_ == k_ ? access_k_frame_ : access_less_k_;
-      frame_id_list.remove(frame_id);
-      frame_info_.erase(frame_id);
-      evictable_num_--;
-    } else {
-      throw Exception("frame_id is not evictable!");
-    }
+  // LOG_INFO("Remove: frame_id: %d", frame_id);
+  if (frame_id > static_cast<int>(replacer_size_)) {
+    throw Exception("Remove:frame_id is invalid!");
+  }
+  if(frame_info_.find(frame_id) == frame_info_.end()) {
+    return;
+  }
+  if (frame_info_[frame_id].evictable_) {
+    std::list<frame_id_t> &frame_id_list = frame_info_[frame_id].access_time_ < k_ ? access_less_k_ : access_k_frame_;
+    frame_id_list.remove(frame_id);
+    frame_info_.erase(frame_id);
+    evictable_num_--;
+  } else {
+    throw Exception("frame_id is not evictable!");
   }
 }
 
