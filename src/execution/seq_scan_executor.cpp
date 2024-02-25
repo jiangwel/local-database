5e46e2ea64a15c5f0319a60c32fcdf4d62e12865
jiangwel <jiangwei9910@gmail.com>
2024-02-25 11:51:43 +0800

    feat:seq scan next

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -18,6 +18,22 @@ SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNod
 
 void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    auto* exec_ctx = this->GetExecutorContext();
+    auto* catalog = exec_ctx->GetCatalog();
+    auto table_oid = this->plan_->GetTableOid();
+    auto* table_metadata = catalog->GetTable(table_oid);
+    if(table_metadata == Catalog::NULL_TABLE_INFO){
+        return false;
+    }
+    auto* table = table_metadata->table_.get();
+    auto it = table->Begin(exec_ctx->GetTransaction());
+    if(it == table->End()){
+        return false;
+    }
+    *tuple = *it;
+    *rid = it->GetRid();
+    return true;
+}
 
 }  // namespace bustub
